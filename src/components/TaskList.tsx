b7bf33112bce4fb689fe1cf23cbe34b474ca99b2
Geovanne Moro <geovannemoro@hotmail.com>
2021-03-10 17:04:20 -0300

    feature null tasks

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, FormEvent } from 'react';
 import { FiTrash, FiCheckSquare } from 'react-icons/fi';
 import { useTransition, animated } from 'react-spring';
 import { v4 } from 'uuid';
@@ -12,6 +12,7 @@ interface Task {
 }
 
 export function TaskList() {
+  const [isLoading, setIsLoading] = useState(false);
   const [tasks, setTasks] = useState<Task[]>(() => {
     const localRepositories = localStorage.getItem('@toDo:tasks');
 
@@ -32,7 +33,7 @@ export function TaskList() {
     localStorage.setItem('@toDo:tasks', JSON.stringify(tasks));
   }, [tasks]);
 
-  function handleCreateNewTask(e: Event) {
+  function handleCreateNewTask(e: FormEvent<HTMLFormElement>) {
     e.preventDefault();
     if (!newTaskTitle) return;
 
@@ -75,38 +76,39 @@ export function TaskList() {
           </button>
         </form>
       </header>
-
       <main>
         <ul>
-          {tasksWithTransitions.map(({ item, key, props }) => (
-            <animated.div key={key} style={props}>
-              <li>
-                <div
-                  className={item.isComplete ? 'completed' : ''}
-                  data-testid="task"
-                >
-                  <label className="checkbox-container">
-                    <input
-                      type="checkbox"
-                      readOnly
-                      checked={item.isComplete}
-                      onClick={() => handleToggleTaskCompletion(item.id)}
-                    />
-                    <span className="checkmark"></span>
-                  </label>
-                  <p>{item.title}</p>
-                </div>
+          {tasks.length === 0 && <span>Sem tarefas no momento :(</span>}
+          {tasks.length !== 0 &&
+            tasksWithTransitions.map(({ item, key, props }) => (
+              <animated.div key={key} style={props}>
+                <li>
+                  <div
+                    className={item.isComplete ? 'completed' : ''}
+                    data-testid="task"
+                  >
+                    <label className="checkbox-container">
+                      <input
+                        type="checkbox"
+                        readOnly
+                        checked={item.isComplete}
+                        onClick={() => handleToggleTaskCompletion(item.id)}
+                      />
+                      <span className="checkmark"></span>
+                    </label>
+                    <p>{item.title}</p>
+                  </div>
 
-                <button
-                  type="button"
-                  data-testid="remove-task-button"
-                  onClick={() => handleRemoveTask(item.id)}
-                >
-                  <FiTrash size={16} />
-                </button>
-              </li>
-            </animated.div>
-          ))}
+                  <button
+                    type="button"
+                    data-testid="remove-task-button"
+                    onClick={() => handleRemoveTask(item.id)}
+                  >
+                    <FiTrash size={16} />
+                  </button>
+                </li>
+              </animated.div>
+            ))}
         </ul>
       </main>
     </section>
