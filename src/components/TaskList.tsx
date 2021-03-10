65bba99553acfb0bfbab4755f0e88077529e7646
Geovanne Moro <geovannemoro@hotmail.com>
2021-03-10 11:59:25 -0300

    Update 1.0

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,7 +1,8 @@
-import { useState } from 'react';
-import '../styles/tasklist.scss';
-
+import { useState, useEffect } from 'react';
 import { FiTrash, FiCheckSquare } from 'react-icons/fi';
+import { useTransition, animated } from 'react-spring';
+
+import '../styles/tasklist.scss';
 
 interface Task {
   id: number;
@@ -10,8 +11,26 @@ interface Task {
 }
 
 export function TaskList() {
-  const [tasks, setTasks] = useState<Task[]>([]);
+  const [tasks, setTasks] = useState<Task[]>(() => {
+    const localRepositories = localStorage.getItem('@toDo:tasks');
+
+    if (localRepositories) {
+      return JSON.parse(localRepositories);
+    }
+
+    return [];
+  });
+  const tasksWithTransitions = useTransition(tasks, (task) => task.id, {
+    from: { left: '-50px', opacity: 0 },
+    enter: { left: '0px', opacity: 1 },
+    leave: { left: '-50px', opacity: 0 },
+  });
+  console.log(tasksWithTransitions);
   const [newTaskTitle, setNewTaskTitle] = useState('');
+
+  useEffect(() => {
+    localStorage.setItem('@toDo:tasks', JSON.stringify(tasks));
+  }, [tasks]);
 
   function handleCreateNewTask() {
     if (!newTaskTitle) return;
@@ -42,7 +61,7 @@ export function TaskList() {
   return (
     <section className="task-list container">
       <header>
-        <h2>Minhas tasks</h2>
+        <h2>Minhas tarefas</h2>
 
         <div className="input-group">
           <input
@@ -63,7 +82,37 @@ export function TaskList() {
 
       <main>
         <ul>
-          {tasks.map((task) => (
+          {tasksWithTransitions.map(({ item, key, props }) => (
+            <animated.div key={key} style={props}>
+              <li>
+                <div
+                  className={item.isComplete ? 'completed' : ''}
+                  data-testid="task"
+                >
+                  <label className="checkbox-container">
+                    <input
+                      type="checkbox"
+                      readOnly
+                      checked={item.isComplete}
+                      onClick={() => handleToggleTaskCompletion(item.id)}
+                    />
+                    <span className="checkmark"></span>
+                  </label>
+                  <p>{item.title}</p>
+                </div>
+
+                <button
+                  type="button"
+                  data-testid="remove-task-button"
+                  onClick={() => handleRemoveTask(item.id)}
+                >
+                  <FiTrash size={16} />
+                </button>
+              </li>
+            </animated.div>
+          ))}
+
+          {/* {tasks.map((task) => (
             <li key={task.id}>
               <div
                 className={task.isComplete ? 'completed' : ''}
@@ -89,7 +138,7 @@ export function TaskList() {
                 <FiTrash size={16} />
               </button>
             </li>
-          ))}
+          ))} */}
         </ul>
       </main>
     </section>
